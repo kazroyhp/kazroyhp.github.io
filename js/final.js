81169225873c9e70dd685c8f10397e8b37a90194
kazroyhp <68785732+kazroyhp@users.noreply.github.com>
2020-07-29 14:56:06 +0900

    Add files via upload

---
+++ js/final.js
@@ -0,0 +1,14 @@
+function yosan(money){
+    
+    
+    var yosan(0) = 8;
+    var yosan(1) = 30;
+    var yosan(2) = 75;
+    var yosan(3) = 50;
+    
+    
+    
+    var messageArray =[yosan(0)+yosan(1)+yosan(2)+yosan(3)];
+    
+        alert("予算は、約" + messageArray[money] + "万円です");
+    }
